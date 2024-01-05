d088b373fa5492bd110457fbf008c298eb89d6ce
Rheinmir <rheinmir@gmail.com>
2024-01-05 07:36:30 +0700

    50%

---
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.noteapp.Adapters.NotesListAdapter;
+import com.example.noteapp.Database.RoomDB;
+import com.example.noteapp.Models.Notes;
+import com.example.noteapp.Models.NotesClickListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    NotesListAdapter notesListAdapter;
+    List<Notes> notes = new ArrayList<>();
+    RoomDB database;
+    FloatingActionButton fab_add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_home);
+        fab_add = findViewById(R.id.fab_add);
+
+        database = RoomDB.getInstance(this);
+        notes = database.mainDAO().getALL();
+
+        updateRecycler(notes);
+
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void updateRecycler(List<Notes> notes) {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
+        notesListAdapter = new NotesListAdapter(MainActivity.this, notes, notesClickListener);
+        recyclerView.setAdapter(notesListAdapter);
+    }
+
+
+    private final NotesClickListener notesClickListener = new NotesClickListener() {
+        @Override
+        public void onClick(Notes notes) {
+
+        }
+
+        @Override
+        public void onLongClick(Notes notes, CardView cardView) {
+
+        }
+    };
+}
